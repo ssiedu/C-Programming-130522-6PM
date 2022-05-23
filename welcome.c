feb59320f86a454cdfa2348974765c8c10b02db9
SSI Digital <42675358+ssiedu@users.noreply.github.com>
2022-05-23 19:04:30 +0530

    welcome

---
+++ welcome.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	system("cls");
+	printf("\nWelcome to ssi digital \n\n");
+	printf("Welcome to c/c++ \n\n");
+	printf("Bhawarkua , Indore");
+	system("pause");
+	
+
+}
